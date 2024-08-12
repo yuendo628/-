1debc281cff2c3f9f97a1b1d6ee98c1ba2d1421d
yuendo628 <info@m-lapine.com>
2024-08-12 09:26:26 +0900

    Add files via upload
    スキルチェック課題②

--- skill_assessment_rust_app/src/main.rs
+++ skill_assessment_rust_app/src/main.rs
@@ -1,27 +1,41 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+fn is_bool(value: String) -> bool {
+    let mut result = false;
+
+    if value.contains("TRUE") {
+        result = true;
+    }
+    else if value.contains("FALSE")
+    {
+        result = true;
+    }
+
+    result
+}
+
 fn main() {
     let path = "sysctl.conf";
     let file = File::open(path).unwrap();
     let reader: BufReader<File> = BufReader::new(file);
-    let mut v: Vec<String> = Vec::new();
 
     for line in reader.lines() {
         let conftxt = line.unwrap();
-
         if conftxt.contains("kernel.endpoint") {
-            v.push(conftxt);
+                if is_bool(conftxt.clone()) == false  {
+                    println!("{}", conftxt.clone());
+            }
         }
         else if conftxt.contains("kernel.debug") {
-            v.push(conftxt);
+            if is_bool(conftxt.clone()) {
+                println!("{}", conftxt.clone());
+            }
         }
         else if conftxt.contains("kernel.log.file") {
-            v.push(conftxt);
+            if is_bool(conftxt.clone()) == false {
+                println!("{}", conftxt.clone());
+            }
         }
     }
-
-    println!("{}", v[0]);
-    println!("{}", v[1]);
-    println!("{}", v[2]);
-    }
+}
